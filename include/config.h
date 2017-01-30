2d3da5d2379eac2bd01e354c8d3bb2c3f2addf8a
Olexander Yermakov <mannavard1611@gmail.com>
2017-01-30 14:15:38 +0200

    Add node name

--- include/config.h
+++ include/config.h
@@ -2,13 +2,21 @@
 #define DIVE_CONFIG_H
 
 #include <string>
+#include <sstream>
 
 namespace dive {
     struct config {
+        std::string name;   // The name of this node. Must be unique in the cluster.
         std::string host;
         unsigned short port;
 
-        config(std::string host, unsigned short port) : host(host), port(port) {};
+        config(std::string host, unsigned short port) : host(host), port(port) {
+            std::stringstream ss;
+            ss << host << ":" << port;
+            name = ss.str();
+        };
+
+        config(std::string host, unsigned short port, std::string name) : host(host), port(port), name(name) {}
     };
 }
 
