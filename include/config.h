f944bbf6b390c9c7d660ba2317a2b3932dcc9e2c
Olexander Yermakov <mannavard1611@gmail.com>
2017-01-30 15:57:51 +0200

    Add config probe_interval

--- include/config.h
+++ include/config.h
@@ -10,13 +10,17 @@ namespace dive {
         std::string host;
         unsigned short port;
 
-        config(std::string host, unsigned short port) : host(host), port(port) {
+        unsigned int probe_interval;   // Interval between probes in milliseconds.
+
+        config(std::string host, unsigned short port, unsigned int probe_interval_ = 200)
+                : host(host), port(port), probe_interval(probe_interval_) {
             std::stringstream ss;
             ss << host << ":" << port;
             name = ss.str();
         };
 
-        config(std::string host, unsigned short port, std::string name) : host(host), port(port), name(name) {}
+        config(std::string host, unsigned short port, std::string name, unsigned int probe_interval_ = 200)
+                : host(host), port(port), name(name), probe_interval(probe_interval_) {}
     };
 }
 
