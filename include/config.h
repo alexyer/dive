9b7cfa1be5d15dbbff8d3df15943896ac5af3af9
Olexander Yermakov <mannavard1611@gmail.com>
2017-02-01 14:31:17 +0200

    Add gossip interval config

--- include/config.h
+++ include/config.h
@@ -13,23 +13,26 @@ namespace dive {
         /// Interval between probes in milliseconds.
         unsigned int probe_interval;
 
+        /// Interval between sending gossips in milliseconds.
+        unsigned int gossip_interval;
+
         /// Retransmit multiplier for the number of retransmissions are attempted for messages broadcast
         /// over gossips.
         unsigned int retransmit_multiplier;
 
-        config(std::string host, unsigned short port, unsigned int probe_interval_ = 200,
-               unsigned int retransmit_multiplier_ = 2)
-                : host(host), port(port), probe_interval(probe_interval_),
-                  retransmit_multiplier(retransmit_multiplier_) {
+        config(std::string host, unsigned short port, unsigned int probe_interval = 200,
+               unsigned int retransmit_multiplier = 2, unsigned int gossip_interval = 200)
+                : host(host), port(port), probe_interval(probe_interval),
+                  retransmit_multiplier(retransmit_multiplier), gossip_interval(gossip_interval) {
             std::stringstream ss;
             ss << host << ":" << port;
             name = ss.str();
         };
 
-        config(std::string host, unsigned short port, std::string name, unsigned int probe_interval_ = 200,
-               unsigned int retransmit_multiplier_ = 2)
-                : host(host), port(port), name(name), probe_interval(probe_interval_),
-                  retransmit_multiplier(retransmit_multiplier_) {}
+        config(std::string host, unsigned short port, std::string name, unsigned int probe_interval = 200,
+               unsigned int retransmit_multiplier = 2)
+                : host{host}, port{port}, name{name}, probe_interval{probe_interval},
+                  retransmit_multiplier{retransmit_multiplier}, gossip_interval{gossip_interval} {}
     };
 }
 
