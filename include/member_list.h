afca47e9b140878333a3080467c1faaae6f223d4
Olexander Yermakov <mannavard1611@gmail.com>
2017-02-07 13:43:17 +0200

    Add MemberList::get_random()

--- include/member_list.h
+++ include/member_list.h
@@ -13,19 +13,29 @@ namespace dive {
      */
     class MemberList {
     public:
+        MemberList();
+
         /**
          * Insert new member.
          */
         void insert(const ClusterMember&);
 
         /**
-         *
+         * Get member by name.
          * @param name Member name.
-         * @return Member
+         * @return ClusterMember
          */
         const ClusterMember& get(std::string name);
+
+        /***
+         * Get random member from the list.
+         * @return ClusterMember
+         */
+        const ClusterMember& get_random() const;
     private:
         std::unordered_map<std::string, ClusterMember> members_;
+        std::random_device rd_;
+        std::mt19937 eng_;
     };
 }
 
--- src/member_list.cpp
+++ src/member_list.cpp
@@ -4,6 +4,8 @@
 
 using namespace dive;
 
+MemberList::MemberList() : eng_(rd_()) {}
+
 void MemberList::insert(const ClusterMember& new_member) {
     if (members_.find(new_member.name) != members_.end()) {
         // TODO(alexyer): Custom exception
@@ -15,4 +17,10 @@ void MemberList::insert(const ClusterMember& new_member) {
 
 const ClusterMember& MemberList::get(std::string name) {
     return members_.at(name);
+}
+
+const ClusterMember& MemberList::get_random() const {
+    std::uniform_int_distribution<> distribution(0, members_.size());
+    auto pair = *std::next(std::begin(members_), distribution(eng_));
+    return pair.second;
 }
